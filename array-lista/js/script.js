5cb5f22fec2cfddfdbc8862a93f957c07b34552e
alegreco7 <alexgreco2002@gmail.com>
2023-11-24 16:51:21 +0100

    aggiunta pesca nell' array frigorifero

--- array-lista/js/script.js
+++ array-lista/js/script.js
@@ -1,2 +1,5 @@
 // Fase 1: Creare l'array con la frutta del frigorifero
 let frigorifero = ['banana', 'mela', 'pera', 'ciliegia', 'arancia', 'mandarino', 'cocomero', 'limone', 'fragola'];
+
+// Fase 2: Aggiungere la pesca all'array della frutta
+frigorifero.push('pesca');
