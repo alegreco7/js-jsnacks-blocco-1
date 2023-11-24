9c618d85532f2c6134eab9361cc3cceb64707be8
alegreco7 <alexgreco2002@gmail.com>
2023-11-24 17:01:38 +0100

    array con numeri casuali

--- array_tail/js/script.js
+++ array_tail/js/script.js
@@ -1,3 +1,8 @@
 // Chiediamo all'utente quanti elementi dovrà contenere l'array
-
 const numeroElementi = parseInt(prompt("Inserisci il numero di elementi dell'array:"));
+
+// Creiamo un array con numeri casuali compresi tra 1 e 100
+let arrayCasuale = [];
+for (let i = 0; i < numeroElementi; i++) {
+    arrayCasuale.push(Math.floor(Math.random() * 100) + 1);
+}
